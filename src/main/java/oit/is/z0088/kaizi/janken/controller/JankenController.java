bf1d7b89cd8c2592702ef7926c9050097aed5184
e1b20015 <e1b20015@oit.ac.jp>
2022-10-22 20:05:50 +0900

    第三回まで完了

--- src/main/java/oit/is/z0088/kaizi/janken/controller/JankenController.java
+++ src/main/java/oit/is/z0088/kaizi/janken/controller/JankenController.java
@@ -1,11 +1,16 @@
 package oit.is.z0088.kaizi.janken.controller;
 
+import java.security.Principal;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import oit.is.z0088.kaizi.janken.model.Entry;
 
 /**
  * Sample21Controller
@@ -15,13 +20,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class JankenController {
 
-  /**
-   * sample21というGETリクエストがあったら sample21()を呼び出し，sample21.htmlを返す
+  @Autowired
+  private Entry entry;
+
+  /*
+   * @GetMapping("/janken")
+   * public String sample21() {
+   * return "janken.html";
+   * }
    */
-  @GetMapping("/janken")
-  public String sample21() {
-    return "janken.html";
-  }
 
   @PostMapping("/janken")
   public String jankenpost(@RequestParam String name, ModelMap model) {
@@ -30,24 +37,33 @@ public class JankenController {
     return "janken.html";
   }
 
-  @GetMapping("/rock/{param1}")
+  @GetMapping("/janken1/{param1}")
   public String rock(@PathVariable String param1, ModelMap model) {
-    String hand = param1;
+    int i = Integer.parseInt(param1);
+    String hand = null, result = null;
+
+    if (i == 1) {
+      hand = "Gu";
+      result = "draw";
+    } else if (i == 2) {
+      hand = "Choki";
+      result = "You Lose";
+    } else if (i == 3) {
+      hand = "Pa";
+      result = "You Win!";
+    }
     model.addAttribute("hand", hand);
+    model.addAttribute("result", result);
+
     return "janken.html";
   }
 
-  @GetMapping("/scissor/{param1}")
-  public String scissor(@PathVariable String param1, ModelMap model) {
-    String hand = param1;
-    model.addAttribute("hand", hand);
-    return "janken.html";
-  }
+  @GetMapping("/janken")
+  public String entryJanken(Principal prin, ModelMap model) {
+    String loginUser = prin.getName();
+    this.entry.addUser(loginUser);
+    model.addAttribute("entry", this.entry);
 
-  @GetMapping("/paper/{param1}")
-  public String paper(@PathVariable String param1, ModelMap model) {
-    String hand = param1;
-    model.addAttribute("hand", hand);
     return "janken.html";
   }
 
