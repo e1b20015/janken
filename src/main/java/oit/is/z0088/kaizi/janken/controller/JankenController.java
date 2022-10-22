eea89f9b44131582e1ed1dd2d22deb4153fa3123
e1b20015 <e1b20015@oit.ac.jp>
2022-10-22 22:58:35 +0900

    DBの初期化

--- src/main/java/oit/is/z0088/kaizi/janken/controller/JankenController.java
+++ src/main/java/oit/is/z0088/kaizi/janken/controller/JankenController.java
@@ -1,8 +1,8 @@
 package oit.is.z0088.kaizi.janken.controller;
 
 import java.security.Principal;
+import java.util.ArrayList;
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import oit.is.z0088.kaizi.janken.model.Entry;
+import oit.is.z0088.kaizi.janken.model.User;
+import oit.is.z0088.kaizi.janken.model.UserMapper;
 
 /**
  * Sample21Controller
@@ -23,6 +25,9 @@ public class JankenController {
   @Autowired
   private Entry entry;
 
+  @Autowired
+  UserMapper userMapper;
+
   /*
    * @GetMapping("/janken")
    * public String sample21() {
@@ -30,12 +35,14 @@ public class JankenController {
    * }
    */
 
-  @PostMapping("/janken")
-  public String jankenpost(@RequestParam String name, ModelMap model) {
-    String username = name;
-    model.addAttribute("username", username);
-    return "janken.html";
-  }
+  /*
+   * @PostMapping("/janken")
+   * public String jankenpost(@RequestParam String name, ModelMap model) {
+   * String username = name;
+   * model.addAttribute("username", username);
+   * return "janken.html";
+   * }
+   */
 
   @GetMapping("/janken1/{param1}")
   public String rock(@PathVariable String param1, ModelMap model) {
@@ -64,6 +71,13 @@ public class JankenController {
     this.entry.addUser(loginUser);
     model.addAttribute("entry", this.entry);
 
+    return "janken.html";
+  }
+
+  @PostMapping("/janken")
+  public String sample45(ModelMap model) {
+    ArrayList<User> user = userMapper.selectAllUser();
+    model.addAttribute("user", user);
     return "janken.html";
   }
 
---
+++ src/main/java/oit/is/z0088/kaizi/janken/model/UserMapper.java
@@ -0,0 +1,42 @@
+package oit.is.z0088.kaizi.janken.model;
+
+import java.util.ArrayList;
+
+//import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+//import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface UserMapper {
+
+  @Select("SELECT id,userName from users where id = #{id}")
+  User selectById(int id);
+
+  @Select("SELECT userName from users;")
+  ArrayList<User> selectAllUser();
+  
+  /**
+   * #{userName}などはinsertの引数にあるChamberクラスのフィールドを表しています 引数に直接String
+   * userNameなどと書いてもいけるはず
+   * 下記のOptionsを指定すると，insert実行時にAuto incrementされたIDの情報を取得できるようになる useGeneratedKeys
+   * = true -> Keyは自動生成されることを表す keyColumn : keyになるテーブルのカラム名 keyProperty :
+   * keyになるJavaクラスのフィールド名
+   *
+   * @param chamber
+   */
+  /*
+   * @Insert("INSERT INTO users (userName) VALUES (#{userName});")
+   *
+   * @Options(useGeneratedKeys = true, keyColumn = "id", keyProperty = "id")
+   * void insertChamber(User user);
+   */
+  /**
+   * DBのカラム名とjavaクラスのフィールド名が同じ場合はそのまま代入してくれる（大文字小文字の違いは無視される）
+   * カラム名とフィールド名が異なる場合の対応も可能だが，いきなり複雑になるので，selectで指定するテーブル中のカラム名とクラスのフィールド名は同一になるよう設計することが望ましい
+   *
+   * @return
+   */
+
+
+}
